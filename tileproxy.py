641ce83f403258f9debeca1c3e22d121ea347136
Ian Dees <ian.dees@gmail.com>
2017-02-12 22:04:30 -0500

    Simple start of tileproxy

---
+++ tileproxy.py
@@ -0,0 +1,244 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+from flask import Flask, abort, flash, jsonify, render_template, send_file
+from geoalchemy2 import Geometry
+from geoalchemy2.shape import to_shape
+from shapely.geometry import mapping
+from pyproj import Proj, transform
+from sqlalchemy.orm import load_only
+from PIL import Image
+from cStringIO import StringIO
+import mercantile
+import requests
+import uuid
+import urlparse
+
+
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class Source(db.Model):
+    __tablename__ = "sources"
+
+    id = db.Column(db.Integer, primary_key=True)
+    slug = db.Column(db.String(80), index=True, unique=True)
+    name = db.Column(db.String(256))
+    url_template = db.Column(db.Text)
+    bbox = db.Column(Geometry(geometry_type='POLYGON', srid=4326), index=True)
+    min_zoom = db.Column(db.Integer)
+    max_zoom = db.Column(db.Integer)
+
+
+class NewEsriSourceForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    url = StringField('url', validators=[DataRequired()])
+
+
+@app.route('/v1/tiles/<layer>/<int:zoom>/<int:x>/<int:y>.png')
+def get_tile(layer, zoom, x, y):
+    (min_lon, min_lat, max_lon, max_lat) = mercantile.bounds(x, y, zoom)
+
+    sources = Source.query.options(load_only(Source.url_template)).filter(
+        Source.min_zoom <= zoom,
+        Source.max_zoom > zoom,
+        Source.bbox.ST_Intersects(
+            'SRID=4326;POLYGON(({min_lon} {min_lat}, {min_lon} {max_lat}, '
+            '{max_lon} {max_lat}, {max_lon} {min_lat}, '
+            '{min_lon} {min_lat}))'.format(
+                min_lon=min_lon,
+                min_lat=min_lat,
+                max_lon=max_lon,
+                max_lat=max_lat,
+            )
+        )
+    )
+    if layer != 'global':
+        sources = sources.filter(
+            Source.slug == layer
+        )
+    sources = sources.all()
+
+    if not sources:
+        abort(404, 'No sources for that tile')
+
+    composite = Image.new('RGBA', (256, 256))
+    for source in sources:
+        print source.name
+        url = source.url_template.format(
+            xmin=min_lon,
+            ymin=min_lat,
+            xmax=max_lon,
+            ymax=max_lat,
+            width=256,
+            height=256,
+        )
+        resp = requests.get(url)
+        resp.raise_for_status()
+        content = StringIO(resp.content)
+        image = Image.open(content)
+        composite.paste(image)
+
+    out_buff = StringIO()
+    composite.save(out_buff, 'PNG', quality=70)
+    out_buff.seek(0)
+
+    return send_file(out_buff, mimetype='image/png')
+
+
+def scale_to_zoom(scale):
+    return None
+
+
+def build_esri_source(name, url):
+    url_parts = urlparse.urlparse(url)
+    proxy_parts = None
+    if url_parts.query:
+        service_parts = urlparse.urlparse(url_parts.query)
+        proxy_parts = url_parts
+    else:
+        service_parts = url_parts
+
+    if service_parts.path.rsplit('/')[-1] not in ('MapServer', 'ImageServer'):
+        raise ValueError("The layer doesn't seem to be a MapServer or ImageServer")
+
+    if proxy_parts:
+        proxied_metadata = urlparse.urlunparse(service_parts._replace(query='f=json'))
+        metadata_parts = url_parts._replace(query=proxied_metadata)
+    else:
+        metadata_parts = url_parts._replace(query='f=json')
+    metadata_url = urlparse.urlunparse(metadata_parts)
+    resp = requests.get(metadata_url)
+
+    if resp.status_code != 200:
+        raise ValueError("Error retrieving layer metadata from " + resp.request.url)
+
+    metadata = resp.json()
+    slug = str(uuid.uuid4())[:8]
+
+    capabilities = metadata.get('capabilities')
+    if url.endswith('ImageServer'):
+        if 'Image' not in capabilities:
+            raise ValueError("The layer doesn't seem to support image export")
+
+        url_template = ''.join([url,
+            ('/exportImage?bbox={xmin},{ymin},{xmax},{ymax}'
+                '&bboxSR=4326&size={width},{height}'
+                '&imageSR=102113&transparent=true'
+                '&format=png&f=image'
+            )
+        ])
+    elif url.endswith('MapServer'):
+        if 'Map' not in capabilities:
+            raise ValueError("The layer doesn't seem to support map export")
+
+        url_template = ''.join([url,
+            ('/export?bbox={xmin},{ymin},{xmax},{ymax}'
+                '&bboxSR=4326&size={width},{height}'
+                '&imageSR=102113&transparent=true'
+                '&format=png&f=image'
+            )
+        ])
+
+    extent = metadata.get('fullExtent')
+    extent_sr = extent.get('spatialReference')
+    from_srid = extent_sr.get('latestWkid') or extent_sr.get('wkid')
+    if from_srid:
+        if from_srid == 102100:
+            from_srid = 3857
+        from_sr = Proj(init='epsg:{}'.format(from_srid))
+    else:
+        from_sr = Proj(extent_sr.get('wkt'), preserve_units=True)
+    to_sr = Proj(init='epsg:4326')
+    (xmin2, ymin2) = transform(from_sr, to_sr, extent.get('xmin'), extent.get('ymin'))
+    (xmax2, ymax2) = transform(from_sr, to_sr, extent.get('xmax'), extent.get('ymax'))
+    bbox = ('SRID=4326;POLYGON(({xmin} {ymin}, {xmin} {ymax}, '
+            '{xmax} {ymax}, {xmax} {ymin}, {xmin} {ymin}))'.format(
+                xmin=xmin2,
+                ymin=ymin2,
+                xmax=xmax2,
+                ymax=ymax2,
+            ))
+
+    source = Source(
+        slug=slug,
+        name=name,
+        url_template=url_template,
+        bbox=bbox,
+        min_zoom=scale_to_zoom(metadata.get('min_scale')) or 0,
+        max_zoom=scale_to_zoom(metadata.get('max_scale')) or 22,
+    )
+
+    return source
+
+
+@app.route('/sources', methods=['GET', 'POST'])
+def show_sources():
+    esri_form = NewEsriSourceForm()
+
+    if esri_form.validate_on_submit():
+        source = build_esri_source(esri_form.name.data, esri_form.url.data)
+        db.session.add(source)
+        db.session.commit()
+
+    sources = Source.query.filter()
+
+    return render_template(
+        'index.html',
+        sources=sources,
+        esri_form=esri_form,
+    )
+
+
+@app.route('/sources/global')
+def show_global():
+    return render_template(
+        'global.html',
+    )
+
+
+@app.route('/sources/<slug>')
+def show_source(slug):
+    source = Source.query.filter_by(slug=slug).first_or_404()
+
+    esri_form = NewEsriSourceForm()
+
+    if esri_form.validate_on_submit():
+        source = build_esri_source(esri_form.name.data, esri_form.url.data)
+        db.session.add(source)
+        db.session.commit()
+
+    return render_template(
+        'show_source.html',
+        source=source,
+        esri_form=esri_form,
+    )
+
+
+@app.route('/sources/<slug>.geojson')
+def show_source_geojson(slug):
+    source = Source.query.filter_by(slug=slug).first_or_404()
+    shape = to_shape(source.bbox)
+    geojson = mapping(shape)
+
+    return jsonify({
+        'type': "Feature",
+        'id': source.slug,
+        'properties': {
+            'name': source.name,
+            'slug': source.slug,
+            'url_template': source.url_template,
+            'min_zoom': source.min_zoom,
+            'max_zoom': source.max_zoom,
+        },
+        'geometry': geojson
+    })
+
+
+if __name__ == '__main__':
+    app.run()
